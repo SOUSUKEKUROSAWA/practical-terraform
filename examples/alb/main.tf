be324771b6e574f4c7d9b1981848245125b158ac
SOUSUKEKUROSAWA <kurosocavs0714@gmail.com>
2024-08-04 17:32:33 +0900

    軽微な修正

--- examples/alb/main.tf
+++ examples/alb/main.tf
@@ -8,7 +8,7 @@ module "example" {
 }
 
 module "vpc" {
-    source = "../../../modules/network/vpc-2az"
+    source = "../../modules/network/vpc-2az"
     cidr_block = "10.0.0.0/16"
     name = "example"
     public_subnet_cidr_blocks = ["10.0.1.0/24", "10.0.2.0/24"]
